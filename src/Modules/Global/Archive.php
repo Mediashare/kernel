a77a9336ffa6d9cd2e85c801e7c55d7f64b9999b
Thibault Marquand - Mediashare <mediashare.supp@gmail.com>
2020-01-11 19:23:08 +0100

    [Debug] Archive

--- src/Modules/Global/Archive.php
+++ src/Modules/Global/Archive.php
@@ -10,7 +10,7 @@ class Archive
     public $source;
     public $destination; 
     public function run() {
-        if (!extension_loaded('zip') || !file_exists($this->directory)) {
+        if (!extension_loaded('zip') || !file_exists($this->destination)) {
             return false;
         }
         $zip = new ZipArchive();
@@ -18,24 +18,24 @@ class Archive
             return false;
         }
     
-        $this->directory = str_replace('\\', '/', realpath($this->directory));
-        $flag = basename($this->directory) . '/';
-        //$zip->addEmptyDir(basename($this->directory) . '/');
+        $this->destination = str_replace('\\', '/', realpath($this->destination));
+        $flag = basename($this->destination) . '/';
+        //$zip->addEmptyDir(basename($this->destination) . '/');
     
-        if (is_dir($this->directory) === true) {
-            $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($this->directory), RecursiveIteratorIterator::SELF_FIRST);
+        if (is_dir($this->destination) === true) {
+            $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($this->destination), RecursiveIteratorIterator::SELF_FIRST);
             foreach ($files as $file) {
                 $file = str_replace('\\', '/', realpath($file));
                 if (is_dir($file) === true) {
-                    $zip->addEmptyDir(str_replace($this->directory . '/', '', $flag.$file . '/'));
+                    $zip->addEmptyDir(str_replace($this->destination . '/', '', $flag.$file . '/'));
                 }
                 else if (is_file($file) === true) {
-                    $zip->addFromString(str_replace($this->directory . '/', '', $flag.$file), file_get_contents($file));
+                    $zip->addFromString(str_replace($this->destination . '/', '', $flag.$file), file_get_contents($file));
                 }
             }
         }
-        else if (is_file($this->directory) === true) {
-            $zip->addFromString($flag.basename($this->directory), file_get_contents($this->directory));
+        else if (is_file($this->destination) === true) {
+            $zip->addFromString($flag.basename($this->destination), file_get_contents($this->destination));
         }
         $zip->close();
         return $this;

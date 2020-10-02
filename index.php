e67457d73add2896cc2d62a51044c723e130db2e
Thibault Marquand - Mediashare <mediashare.supp@gmail.com>
2020-10-02 14:46:07 +0200

    CloudFlare ByPass

--- index.php
+++ index.php
@@ -3,7 +3,11 @@ require 'vendor/autoload.php';
 use Mediashare\Kernel\Kernel;
 
 $kernel = new Kernel();
-dump($kernel);
+// dump($kernel);
+
+$request = $kernel->get('Curl');
+$response = $request->get('https://www2.yggtorrent.si/torrent/filmvid%C3%A9o/film/667716-de+toutes+nos+forces+2013+french+brrip+xvid+ac3-notag');
+var_dump($response);die;
 
 $hello = $kernel->get('Hello');
 $hello->echo("Test one module \n");
